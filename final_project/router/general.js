b67bac4077a7e206969ee1040e248ea03a411270
Levy70 <levis-mbaku.akanji@student.nbi-handelsakademin.se>
2024-09-03 23:47:15 +0200

    get books using Async-Await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,36 +1,48 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
-let isValid = require("./auth_users.js").isValid;
-let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 const doesExist = (username) => {
     return users.some((user) => user.username === username);
 };
 
-// Register a new user
+// User registration route
 public_users.post("/register", (req, res) => {
     const { username, password } = req.body;
+
     if (username && password) {
         if (!doesExist(username)) {
-            users.push({"username": username, "password": password});
-            return res.status(200).json({message: "User successfully registered. Now you can login"});
+            users.push({ "username": username, "password": password });
+            return res.status(200).json({ message: "User successfully registered. Now you can login" });
         } else {
-            return res.status(404).json({message: "User already exists!"});
+            return res.status(404).json({ message: "User already exists!" });
         }
     }
-    return res.status(404).json({message: "Unable to register user."});
+    return res.status(404).json({ message: "Unable to register user." });
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  res.send(JSON.stringify(books, null, 4));
+// Get the book list available in the shop using Async-Await
+public_users.get('/', async function (req, res) {
+    try {
+        // Simulate an API call to fetch books (using the in-memory 'books' object)
+        const booksList = await axios.get('http://localhost:5000/booksdb');
+        res.status(200).json(booksList.data);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching books", error });
+    }
+});
+
+// Endpoint to serve the books data
+public_users.get('/booksdb', function (req, res) {
+    res.status(200).json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
     const isbn = req.params.isbn;
     const foundBook = Object.values(books).find(book => book.isbn === isbn);
+
     if (foundBook) {
         res.status(200).send(foundBook);
     } else {
@@ -42,6 +54,7 @@ public_users.get('/isbn/:isbn', function (req, res) {
 public_users.get('/author/:author', function (req, res) {
     const author = req.params.author;
     const booksByAuthor = Object.values(books).filter(book => book.author === author);
+
     if (booksByAuthor.length > 0) {
         res.status(200).send(booksByAuthor);
     } else {
@@ -53,6 +66,7 @@ public_users.get('/author/:author', function (req, res) {
 public_users.get('/title/:title', function (req, res) {
     const title = req.params.title;
     const booksByTitle = Object.values(books).filter(book => book.title === title);
+
     if (booksByTitle.length > 0) {
         res.status(200).send(booksByTitle);
     } else {
@@ -64,40 +78,11 @@ public_users.get('/title/:title', function (req, res) {
 public_users.get('/review/:isbn', function (req, res) {
     const isbn = req.params.isbn;
     const foundBook = Object.values(books).find(book => book.isbn === isbn);
+
     if (foundBook) {
         res.status(200).send(foundBook.reviews);
     } else {
         res.status(404).send({ message: 'Book not found' });
-    }
-});
-
-// Add or modify a book review
-public_users.post('/review/:isbn', function (req, res) {
-    const isbn = req.params.isbn;
-    const review = req.query.review;
-    const username = req.session.username; // Assuming the username is stored in the session
-
-    // Check if the user is logged in
-    if (!username) {
-        return res.status(401).json({ message: 'You need to be logged in to post a review.' });
-    }
-
-    // Find the book by ISBN
-    const foundBook = Object.values(books).find(book => book.isbn === isbn);
-
-    if (foundBook) {
-        // Check if the user has already posted a review for this book
-        if (foundBook.reviews[username]) {
-            // Update the existing review
-            foundBook.reviews[username] = review;
-            return res.status(200).json({ message: 'Review updated successfully.' });
-        } else {
-            // Add a new review
-            foundBook.reviews[username] = review;
-            return res.status(200).json({ message: 'Review added successfully.' });
-        }
-    } else {
-        return res.status(404).json({ message: 'Book not found' });
     }
 });
 
